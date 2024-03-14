25a5abedda2c57c59dbf4c06561858c67537604f
nittan <nitin01kumar14@gmail.com>
2024-03-14 10:54:21 +0530

    1

---
+++ src/FunctionalInterface/Main.java
@@ -0,0 +1,30 @@
+package FunctionalInterface;
+
+
+// we use lambda experssion with functional interfaces
+@FunctionalInterface
+interface A{
+    void show();
+}
+
+class B implements A{
+    public void show(){
+        System.out.println("in show of class B");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A() {
+            @Override
+            public void show() {
+                System.out.println(" in show");
+            }
+        };
+        obj.show();
+
+        A obj2 = new B();
+        obj2.show();
+
+    }
+}
---
+++ src/InnerClass/Main.java
@@ -0,0 +1,44 @@
+package InnerClass;
+
+class Outer{
+    int a = 10;
+    public void method1(){
+        System.out.println("Outer method");
+    }
+
+
+//    class Inner{
+    static class Inner{ // if static we can directly create object of it
+        public void method2(){
+            System.out.println("Inner method");
+        }
+        int b = 50;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        System.out.println(outer.a);
+        outer.method1();
+
+//        Outer.Inner inner  = new Inner();
+        // error because we cannot create object of inner class without an
+        // object of outer class like we cannot access a variable without object of outer
+
+//        Outer.Inner inner = new Outer.Inner();
+        // still error beause there is not object of outer is used
+
+
+//        Outer.Inner inner = outer.new Inner();
+//        System.out.println(inner.b);
+//        inner.method2();
+
+        // we can make class inner static and then we can create object of it
+        Outer.Inner inner = new Outer.Inner();
+        System.out.println(inner.b);
+        inner.method2();
+
+
+    }
+}
